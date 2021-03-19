05fc6f70c4ae5b8587bae9ff0b765f792a4b8e9e
unknown <claudinei.pr@gmail.com>
2021-03-19 20:54:23 -0300

    ajustes e finalizacao do jogo

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let food = {
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16*box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
@@ -43,12 +43,20 @@ function update(event){
 }
 
 function iniciarJogo(){
-
+    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
+    for(i = 1;i<snake.length;i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -69,7 +77,7 @@ function iniciarJogo(){
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
-    
+
     let newHead = {
         x: snakeX,
         y: snakeY
